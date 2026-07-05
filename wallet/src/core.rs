5e77654ee59430679f1db4488013ae162b6656eb
Hugo Sanchez <hugodoesstock@gmail.com>
2026-07-05 15:39:31 -0400

    Initial commit: itx workspace

--- wallet/src/core.rs
+++ wallet/src/core.rs
@@ -1,5 +1,6 @@
 use anyhow::Result;
 use crossbeam_skiplist::SkipMap;
+use kanal::AsyncSender;
 use serde::{Deserialize, Serialize};
 use tokio::net::TcpStream;
 use std::fs;
@@ -9,6 +10,152 @@ use btclib::crypto::{PrivateKey, PublicKey};
 use btclib::network::Message;
 use btclib::types::{Transaction, TransactionOutput};
 use btclib::util::Saveable;
+
+
+#[derive(Clone)]
+struct UtxoStore {
+    my_keys: Vec<LoadedKey>,
+    utxos: Arc<SkipMap<PublicKey, Vec<(bool, TransactionOutput)>>>,
+
+}
+impl UtxoStore {
+    fn new() -> Self {
+        UtxoStore {
+            my_keys: Vec::new(),
+            utxos: Arc::new(SkipMap::new()), //SkipMap is the cocurrent alternative to the binary tree map, in the same way that the DashMap is the cocurrency alternative of the Hashmap
+        }
+    }
+
+    fn add_key(&mut self, key: LoadedKey) {
+        self.my_keys.push(key);
+    }
+}
+
+#[derive(Clone)]
+pub struct Core {
+    pub config: Config,
+    utxos: UtxoStore,
+    pub tx_sender: AsyncSender<Transaction>,
+}
+
+impl Core {
+    fn new(config: Config, utxos: UtxoStore) -> Self {
+        let (tx_sender, _) = kanal::bounded(10);
+        Core {
+            config,
+            utxos,
+            tx_sender: tx_sender.clone_async(),
+        }
+    }
+    pub fn load(config_path: PathBug) -> Result<Self> {
+        let config: Config = toml::from_str(&fs::read_to_string(&config_path)?)?;
+        let mut utxos = UtxoStore::new();
+        // Load keys from config
+        for key in &config.my_keys {
+            let public = PublicKey::load_from_file(&key.public)?;
+            let private = PrivateKey::load_from_file(&key.private)?;
+            utxos.add_key(Loaded { public, private });
+        }
+        Ok(Core::new(config, utxos))
+    }
+
+    pub async fn fetch_utxos(&self) -> Result<()>{
+        TcpStream::connect(&self.config.default_node).await?;
+        for key in &self.utxos.my_keys {
+            let message = Message::FetchUTXOs(key.public.clone());
+            message.send_async(&mut stream).await?;
+            if let Message::UTXOs(utxos) = Message::receive_async(&mut stream).await?{
+                // Replace the entire UTXO set for this key
+                self.utxos.utxos.insert(key.public.clone(), utxos.into_iter().map(|(output, marked)| (marked, output)).collect());
+            } else {
+                return Err(anyhow::anyhow!("Unexpected response from node"));
+            }
+        }
+        Ok(())
+    }
+
+    pub async fn send_transaction (&self, transaction) -> Result<()>{
+        let mut stream =TcpStream::connect(&self.config.default_node).await?;
+        let message = Message::SubmitTransaction(transaction);
+        message.send_async(&mut stream).await?;
+        Ok(())
+    }
+
+    pub fn get_balance(&self) -> u64 {
+        self.utxos
+           .utxos
+           .iter()
+           .map(|entry| {entry.value().iter().map(|utxo| utxo.1.value).sum::<u64>()}).sum()
+    }
+
+    pub async fn create_transaction (&self, recipient: &PublicKey, amount: u64) -> Result<Transaction> {
+        let fee = self.calculate_fee(amount)
+        let total_amount = amount + fee;
+        let mut inputs = Vec::new();
+        let mut input_sum = 0;
+        for entry in self.utxos.utxos.iter() {
+            let pubkey = entry.key();
+            let utxos = entry.value();
+            for (marked, utxo) in utxos.iter() {
+                if *marked {
+                    continue; //skip marked UTXOs
+                }
+                if input_sum >= total_amount {
+                    break; //invalid 
+                }
+                inputs.push(btclib::types::TransactionInput {
+                    prev_transaction_output_hash: utxo.hash(),
+                    signature: btclib::crypto::Signature::sign_output(
+                        &utxo.hash(),
+                        &self.utxo.my_keys.iter().find(|k| k.public == *pubkey).unwrap().private
+                    ),
+                });
+                input_sum += utxo.value;
+            }
+            if input_sum >= total_amount {
+                break;
+            }
+        }
+        if input_sum < total_amount {
+            return Err(anyhow::anyhow!("Insufficient funds"));
+        }
+        let mut ouputs = vec![TransactionOutput {
+            value: amount,
+            unique_id: uuid::Uuid::new_v4(),
+            pubkey: self.utxos.my_keys[0].public.clone(),
+        }];
+        
+        if input_sum > total_amount {
+            outputs.push(TransactionOutput {
+                value: input_sum - total_amount, 
+                unique_id: uuid::Uuid::new_v4(),
+                pubkey: self.utxos.my_keys[0].public.clone(),
+            });
+        }
+
+        Ok(Transaction::new(inputs, outputs))
+
+
+    }
+
+
+    fn calculate_fee(&self, amount: u64) -> u64 {
+        match self.config.fee_config.fee_type {
+            FeeType::Fixed => {
+                self.config.fee_config.value as u64
+            }
+            FeeType::Percent => {
+                (amount as f64 * self.config.fee_config.value / 100.0) as u64
+            }
+        }
+    }
+
+
+
+}
+
+
+
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Key {
     public: PathBuf,
